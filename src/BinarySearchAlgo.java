d2a0d17116c7fd313946c5b0bd3c2585dd1aecff
vinit golani <vinit.21bce8373@vitapstudent.ac.in>
2024-03-15 01:34:26 +0530

    changes made

--- src/BinarySearchAlgo.java
+++ src/BinarySearchAlgo.java
@@ -2,7 +2,7 @@ public class BinarySearchAlgo {
     public static void main(String[] args) {
         int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
         int target = 22;
-        int ans = binarysearch(arr, 2);
+        int ans = binarysearch(arr, target);
         System.out.println(ans);
     }
 
